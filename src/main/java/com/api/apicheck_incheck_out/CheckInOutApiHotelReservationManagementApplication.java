0da19a67c284ece41e57b471fcdfccc615101094
Sara-Akharraz <saraakharraz22@gmail.com>
2025-04-06 07:24:28 +0000

    update

--- src/main/java/com/api/apicheck_incheck_out/CheckInOutApiHotelReservationManagementApplication.java
+++ src/main/java/com/api/apicheck_incheck_out/CheckInOutApiHotelReservationManagementApplication.java
@@ -1,9 +1,11 @@
-package com.example.demo;
+package com.api.apicheck_incheck_out;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition
 public class CheckInOutApiHotelReservationManagementApplication {
 
 	public static void main(String[] args) {
