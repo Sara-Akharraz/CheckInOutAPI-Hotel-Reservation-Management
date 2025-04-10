cd101f38215d7a1ddb1fe992891aabac41e9b220
Sara-Akharraz <saraakharraz22@gmail.com>
2025-04-10 06:53:01 +0000

    DocumentScanRepository Update

--- src/main/java/com/api/apicheck_incheck_out/DocumentScanMock/Repository/DocumentScanRepository.java
+++ src/main/java/com/api/apicheck_incheck_out/DocumentScanMock/Repository/DocumentScanRepository.java
@@ -2,6 +2,8 @@ package com.api.apicheck_incheck_out.DocumentScanMock.Repository;
 
 import com.api.apicheck_incheck_out.DocumentScanMock.Entity.DocumentScan;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DocumentScanRepository extends JpaRepository<DocumentScan,Long> {
 }
