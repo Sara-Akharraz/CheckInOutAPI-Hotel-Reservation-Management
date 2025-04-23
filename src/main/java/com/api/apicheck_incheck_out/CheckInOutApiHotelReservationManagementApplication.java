f0cb0db5743041fd60f6325efe483f8fec3d3e61
Sara-Akharraz <saraakharraz22@gmail.com>
2025-04-23 10:32:24 +0000

    update

--- src/main/java/com/api/apicheck_incheck_out/CheckInOutApiHotelReservationManagementApplication.java
+++ src/main/java/com/api/apicheck_incheck_out/CheckInOutApiHotelReservationManagementApplication.java
@@ -1,23 +1,23 @@
 package com.api.apicheck_incheck_out;
 
+import com.api.apicheck_incheck_out.Service.Impl.EmailSenderService;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-		(
-				scanBasePackages = "com.api.apicheck_incheck_out"
-		)
-@EnableJpaRepositories(basePackages = "com.api.apicheck_incheck_out")
-@EntityScan(basePackages = "com.api.apicheck_incheck_out")
-
 @OpenAPIDefinition
 public class CheckInOutApiHotelReservationManagementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CheckInOutApiHotelReservationManagementApplication.class, args);
 	}
+
+
 
 }
